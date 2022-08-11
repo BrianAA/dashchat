20038c8c421af988cad95d933e92f3cf19976d39
LionRichter <57396392+LionRichter@users.noreply.github.com>
2022-08-11 23:45:19 +0200

    DashChatTester.cs
     debug/test DashChat without setting up a GUI.

---
+++ DashChatTester.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Quick way to debug/test DashChat changes with minimal setup. Just add this to a GameObject, define demoText and you are set.
+/// </summary>
+public class DashChatTester : MonoBehaviour
+{
+    public TextAsset demoText;
+
+    private DashChat dashChat;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            dashChat.NextLine();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            dashChat.SelectOptions(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            dashChat.SelectOptions(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            dashChat.SelectOptions(2);
+        }
+        //TODO: Add options 3..9?
+    }
+    
+    private void OnEnable()
+    {
+        dashChat = DashChat.instance;
+        SubscribeDashChatEvents();
+        dashChat.InitializeChat(demoText);
+    }
+    
+    private void OnDisable()
+    {
+        UnsubscribeDashChatEvents();
+    }
+
+    private void SubscribeDashChatEvents()
+    {
+        Output("Subscribing to Events");
+
+        DashChat.OnDisplayChat += DashChat_OnDisplayChat;
+        DashChat.OnDisplayOptions += DashChat_OnDisplayOptions;
+        DashChat.OnTriggerEvent += DashChat_OnTriggerEvent;
+        DashChat.OnLookUpVariable += DashChat_OnLookUpVariable;
+        DashChat.OnChangedState += DashChat_OnChangedState;
+        DashChat.OnActorSwitch += DashChat_OnActorSwitch;
+    }
+
+    private void UnsubscribeDashChatEvents()
+    {
+        Output("Unsubscribing Events");
+
+        DashChat.OnDisplayChat -= DashChat_OnDisplayChat;
+        DashChat.OnDisplayOptions -= DashChat_OnDisplayOptions;
+        DashChat.OnTriggerEvent -= DashChat_OnTriggerEvent;
+        DashChat.OnLookUpVariable -= DashChat_OnLookUpVariable;
+        DashChat.OnChangedState -= DashChat_OnChangedState;
+        DashChat.OnActorSwitch -= DashChat_OnActorSwitch;
+    }
+
+    private void DashChat_OnDisplayOptions(List<Option> obj)
+    {
+        var optionStrings = new List<string>();
+
+        for (int i = 0; i < obj.Count; i++)
+        {
+            optionStrings.Add($"{i}: {obj[i].text}");
+        }
+
+        Output($"options: {string.Join(Environment.NewLine, optionStrings)}");
+    }
+    
+    private void DashChat_OnDisplayChat(string obj)
+    {
+        Output($"text: {obj}");
+    }
+
+    private void DashChat_OnActorSwitch(string obj)
+    {
+        Output($"switched actor to: {obj}");
+    }
+
+    private void DashChat_OnChangedState(DSState obj)
+    {
+        Output($"changed state to {obj}");
+    }
+
+    private void DashChat_OnLookUpVariable(string obj)
+    {
+        Output($"triggered variable lookup: {obj}");
+
+        DashChat.currentVariable = $"DemoVariable:{obj}";
+    }
+
+    private void DashChat_OnTriggerEvent(string obj)
+    {
+        Output($"triggered custom event: {obj}");
+    }
+
+    private void Output(string str)
+    {
+        Debug.Log($"DashChatTester> {str}");
+    }
+}
